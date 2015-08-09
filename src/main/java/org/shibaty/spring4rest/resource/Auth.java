9d3af1955cfbe7684f8b0516c30d6f60896f595b
Yasutaka <Yasutaka@192.168.1.4>
2015-08-10 03:37:23 +0900

    add lombok

--- src/main/java/org/shibaty/spring4rest/resource/Auth.java
+++ src/main/java/org/shibaty/spring4rest/resource/Auth.java
@@ -6,13 +6,22 @@ package org.shibaty.spring4rest.resource;
 
 import java.io.Serializable;
 
-import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * @author Yasutaka
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Auth implements Serializable {
 
   public String userId;
@@ -21,77 +30,5 @@ public class Auth implements Serializable {
 
   public String AuthenticationToken;
 
-  /**
-   *
-   */
-  public Auth() {
-    // nop
-  }
-
-  /**
-   * @param userId
-   * @param password
-   * @param authenticationToken
-   */
-  public Auth(String userId, String password, String authenticationToken) {
-    this.userId = userId;
-    this.password = password;
-    AuthenticationToken = authenticationToken;
-  }
-
-  /**
-   * @return userId
-   */
-  @XmlElement(required=false)
-  public String getUserId() {
-    return userId;
-  }
-
-  /**
-   * @param userId セットする userId
-   */
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
-
-  /**
-   * @return password
-   */
-  @XmlElement(required=false)
-  public String getPassword() {
-    return password;
-  }
-
-  /**
-   * @param password セットする password
-   */
-  public void setPassword(String password) {
-    this.password = password;
-  }
-
-  /**
-   * @return authenticationToken
-   */
-  @XmlElement(required=false)
-  public String getAuthenticationToken() {
-    return AuthenticationToken;
-  }
-
-  /**
-   * @param authenticationToken セットする authenticationToken
-   */
-  public void setAuthenticationToken(String authenticationToken) {
-    AuthenticationToken = authenticationToken;
-  }
-
-  /*
-   * (非 Javadoc)
-   * @see java.lang.Object#toString()
-   */
-  @Override
-  public String toString() {
-    return "Auth [userId=" + userId + ", password=" + password + ", AuthenticationToken="
-        + AuthenticationToken + "]";
-  }
 
 }
